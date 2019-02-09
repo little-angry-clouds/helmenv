03fe7952ffa411b4b89c2b2612d79914e0e4a72a
Alex Perez-Pujol <alexperezpujol@disroot.org>
2019-02-09 22:44:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,110 @@
+# helmenv
+[Helm](https://helm.sh) version manager inspired by
+[tfenv](https://github.com/tfutils/tfenv/). It also manages the plugins, charts
+and others. It basically assigns the variable `HELM_HOME`, which by default
+points to `$HOME/.helm/`, to the path `$HOME/.helm/$version`.
+
+## Support
+Currently helmenv supports the following OSes
+- Mac OS X (64bit) - not really tested
+- Linux
+  - 32bit
+  - 64bit
+  - Arm
+  - Arm64
+
+## Installation
+1. Check out helmenv into any path (`${HOME}/.helmenv` in the example)
+
+```bash
+git clone https://github.com/alexppg/helmenv.git ~/.helmenv
+```
+
+2. Add `~/.bin` to your `$PATH`
+
+```bash
+echo 'export PATH="$HOME/.bin:$PATH"' >> ~/.bashrc
+# Or
+echo 'export PATH="$HOME/.bin:$PATH"' >> ~/.zshrc
+```
+
+3. Source the script
+```bash
+echo 'source $HOME/.helmenv/helmenv.sh' >> ~/.bashrc
+# Or
+echo 'source $HOME/.helmenv/helmenv.sh' >> ~/.zshrc
+```
+
+## Usage
+### helmenv help
+
+``` bash
+$ helmenv help
+Usage: helmenv <command> [<options>]
+Commands:
+    list-remote   List all installable versions
+    list          List all installed versions
+    install       Install a specific version
+    use           Switch to specific version
+    uninstall     Uninstall a specific version
+```
+
+### helmenv list-remote
+List installable versions:
+
+```bash
+$ helmenv list-remote
+Fetching versions...
+v1.2.1
+v2.0.0
+v2.0.1
+v2.0.2
+...
+```
+
+### helmenv list
+List installed versions:
+```bash
+$ helmenv list
+v2.10.0
+v2.11.0
+v2.12.3
+```
+
+### helmenv install
+Install a specific version:
+
+```bash
+$ helmenv install v2.10.0
+Creating $HOME/.helm/v2.10.0
+Creating $HOME/.helm/v2.10.0/repository
+Creating $HOME/.helm/v2.10.0/repository/cache
+Creating $HOME/.helm/v2.10.0/repository/local
+Creating $HOME/.helm/v2.10.0/plugins
+Creating $HOME/.helm/v2.10.0/starters
+Creating $HOME/.helm/v2.10.0/cache/archive
+Creating $HOME/.helm/v2.10.0/repository/repositories.yaml
+Adding stable repo with URL: https://kubernetes-charts.storage.googleapis.com
+Adding local repo with URL: http://127.0.0.1:8879/charts
+$HELM_HOME has been configured at $HOME/.helm/v2.10.0.
+Not installing Tiller due to 'client-only' flag having been set
+Happy Helming!
+```
+
+### helmenv use
+Switch to specific version:
+
+```bash
+$ helmenv use
+Done! Now helm points to the v2.11.0 version
+```
+
+### helmenv uninstall
+Uninstall a specific version:
+```bash
+$ helmenv uninstall 0.7.0
+The version v2.12.3 is uninstalled!
+```
+
+## License
+GPL3
---
+++ helmenv.sh
@@ -0,0 +1,221 @@
+#!/usr/bin/env bash
+
+function _helmenv_test_requirements {
+    if [[ ! "$(command -v curl)" ]]
+    then
+        echo "You must install curl"
+        exit 1
+    elif [[ ! "$(command -v jq)" ]]
+    then
+         echo "You must install jq"
+         exit 1
+    fi
+}
+
+function _helmenv_get_so_and_arch(){
+    local _uname
+    local _arch
+
+    _uname="$(uname -s)"
+    _arch="$(arch)"
+
+    case "${_uname}" in
+        Linux)  machine=linux;;
+        Darwin) machine=darwin;;
+        *)      machine="UNKNOWN:${_uname}"
+    esac
+
+    case "${_arch}" in
+        i386)   architecture="386";;
+        arm)    architecture="arm";;
+        arm64)  architecture="arm64";;
+        x86_64) architecture="amd64";;
+        *)      architecture="UNKNOWN:${_arch}"
+    esac
+
+    if [[ "$machine" = "darwin" && "$architecture" != "amd64" ]]
+    then
+        echo "Mac only supports amd64..."
+        return 1
+    fi
+
+    echo "$machine-$architecture"
+}
+
+function helmenv_list_remote () {
+    echo "Fetching versions..."
+    # TODO Paginate over this url
+    versions_url="https://api.github.com/repos/helm/helm/releases?per_page=100"
+    versions="$(curl -s "$versions_url" | jq -r ".[].tag_name" | grep -v "rc\\|beta\\|alpha" | sort --version-sort)"
+    echo "$versions"
+    return 0
+}
+
+function helmenv_install () {
+    VERSION="$1"
+
+    if [[ -z "$VERSION" ]]
+    then
+        echo "You must specify a version!"
+        return 1
+    fi
+
+    if [[ -e "$HELM_BINARY_PATH/helm-$VERSION" ]]
+    then
+        echo "The version $VERSION is already installed!"
+        return 0
+    fi
+
+    os_arch=$(_helmenv_get_so_and_arch)
+    if [[ "$os_arch" = *"UNKNOWN"* ]]
+    then
+        echo "The architecture and/or the SO is not supported: $os_arch"
+        return 1
+    fi
+
+    url="https://storage.googleapis.com/kubernetes-helm/helm-$VERSION-$os_arch.tar.gz"
+    echo "Downloading binary..."
+    curl -s -L -o "/tmp/helm-$VERSION.tar.gz" "$url"
+
+    filetype="$(file -b "/tmp/helm-$VERSION.tar.gz")"
+    if [[ "$filetype" != *"gzip"* ]]
+    then
+        echo "There was a problem downloading the file! You probably typed the version incorrectly, but it may be something else."
+        return 1
+    fi
+
+    mkdir -p /tmp/helm
+    tar -zxf "/tmp/helm-$VERSION.tar.gz" -C /tmp/helm
+    mv /tmp/helm/linux-amd64/helm "$HELM_BINARY_PATH/helm-$VERSION"
+    rm -r /tmp/helm
+
+    if [[ -L "$HELM_BINARY_PATH/helm" ]]
+    then
+        actual_version="$(basename "$(readlink -f "$HELM_BINARY_PATH/helm")")"
+        echo "helm is pointing to the ${actual_version//helm-} version"
+        echo "Do you want to overwrite it? (y/n)"
+        read -r overwrite
+        if [[ "$overwrite" == "y" ]]
+        then
+            helmenv_use "$VERSION"
+        else
+            echo "Nothing done, helm still points to the ${actual_version//helm-} version"
+        fi
+    else
+        helmenv_use "$VERSION"
+    fi
+
+    if [[ "$overwrite" == "y" ]]
+    then
+        "$HELM_BINARY_PATH/helm-$VERSION" init --client-only
+    else
+        HELM_HOME="$HOME/.helm/$VERSION" "$HELM_BINARY_PATH/helm-$VERSION" init --client-only
+    fi
+}
+
+function helmenv_uninstall(){
+    VERSION="$1"
+
+    if [[ -z "$VERSION" ]]
+    then
+        echo "You must specify a version!"
+        return 1
+    fi
+
+    if [[ -e "$HELM_BINARY_PATH/helm-$VERSION" ]]
+    then
+        rm "$HELM_BINARY_PATH/helm-$VERSION"
+        if [[ -e "$HOME/.helm/$VERSION" ]]
+        then
+           rm -r "$HOME/.helm/$VERSION"
+        fi
+        echo "The version $VERSION is uninstalled!"
+    else
+        echo "Nothing done, the version $VERSION is not installed!"
+    fi
+}
+
+function helmenv_list(){
+    installed_versions="$(find "${HELM_BINARY_PATH}" -name '*helm*' -printf '%f\n' | sed -r 's/helm-?//' | sed '/^$/d' | sort --version-sort)"
+    echo "$installed_versions"
+}
+
+function helmenv_use(){
+    VERSION="$1"
+
+    if [[ -z "$VERSION" ]]
+    then
+        echo "You must specify a version!"
+        return 1
+    fi
+
+    installed="$(find "$HELM_BINARY_PATH" -name "*$VERSION*")"
+
+    if [[ -z "$installed" ]]
+    then
+        echo "The $VERSION version is not installed!"
+        return 1
+    fi
+
+    actual_link="$(readlink -f "$HELM_BINARY_PATH/helm")"
+
+    if [[ "$actual_link" =~ $VERSION ]]
+    then
+        echo "helm was already pointing to the $VERSION version!"
+    else
+        ln -sf "$HELM_BINARY_PATH/helm-$VERSION" "$HELM_BINARY_PATH/helm"
+        echo "Done! Now helm points to the $VERSION version"
+        export HELM_HOME="$HOME/.helm/${VERSION}"
+    fi
+}
+
+function helmenv_help() {
+    echo "Usage: helmenv <command> [<options>]"
+    echo "Commands:"
+    echo "    list-remote   List all installable versions"
+    echo "    list          List all installed versions"
+    echo "    install       Install a specific version"
+    echo "    use           Switch to specific version"
+    echo "    uninstall     Uninstall a specific version"
+}
+
+function helmenv_init () {
+    HELM_BINARY_PATH="$HOME/.bin"
+    ACTUAL_VERSION="$(basename "$(readlink -e "$HELM_BINARY_PATH/helm")")"
+    HELM_HOME="$HOME/.helm/${ACTUAL_VERSION//helm-}"
+
+    export HELM_HOME
+
+    if [[ ! -e "$HELM_HOME" ]]
+    then
+        mkdir "$HELM_HOME"
+    fi
+    if [[ ! -e "$HELM_BINARY_PATH" ]]
+    then
+        mkdir "$HELM_BINARY_PATH"
+    fi
+}
+
+function helmenv() {
+    ACTION="$1"
+    ACTION_PARAMETER="$2"
+
+    _helmenv_test_requirements
+
+    case "${ACTION}" in
+        "list-remote")
+            helmenv_list_remote;;
+        "list")
+            helmenv_list;;
+        "install")
+            helmenv_install "$ACTION_PARAMETER";;
+        "uninstall")
+            helmenv_uninstall "$ACTION_PARAMETER";;
+        "use")
+            helmenv_use "$ACTION_PARAMETER";;
+        *)
+            helmenv_help
+    esac
+}
+
+helmenv_init
