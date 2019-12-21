f8a86a4816f64c9138112063b734f9ecae832056
Sebastian Szary <sebastian@szary.org>
2019-12-22 00:11:45 +0100

    Instead of exit 0, return 0 works better. Added checking of rc from _helmenv_test_requirements

--- helmenv.sh
+++ helmenv.sh
@@ -4,18 +4,18 @@ function _helmenv_test_requirements {
     if [[ ! "$(command -v curl)" ]]
     then
         echo "helmenv: You must install curl"
-        exit 0
+        return 0
     elif [[ ! "$(command -v jq)" ]]
     then
          echo "helmenv: You must install jq"
-         exit 0
+         return 0
     fi
 
     # macOS: verify greadlink installed
     if [[ "$HELM_OS_ARCH" == "darwin"* ]]; then
         if [[ ! "$(command -v greadlink)" ]]; then
             echo "helmenv: You must install coreutils"
-            exit 0
+            return 0
         fi
     fi
 }
@@ -219,6 +219,7 @@ function helmenv_help() {
 function helmenv_init () {
     HELM_BINARY_PATH="${HELM_BINARY_PATH:-$HOME/.helm/bin}"
     HELM_OS_ARCH="$(_helmenv_get_os_and_arch)"
+    _helmenv_test_requirements && [[ $? = 0 ]] && return 0
     if [[ "$HELM_OS_ARCH" == "darwin"* ]]; then
         ACTUAL_VERSION="$(basename "$(greadlink -e "$HELM_BINARY_PATH/helm")")"
     else
@@ -249,7 +250,7 @@ function helmenv() {
     ACTION="$1"
     ACTION_PARAMETER="$2"
 
-    _helmenv_test_requirements
+    _helmenv_test_requirements && [[ $? = 0 ]] && return 0
 
     case "${ACTION}" in
         "list-remote")
